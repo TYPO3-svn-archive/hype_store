fda4f4e2a43493ac67a01786ccfc8f5d9573dcc7
thasmo <thasmo@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-11 11:26:17 +0000

    Fixes #12961.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hype_store/trunk@43460 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Domain/Repository/ProductPriceRepository.php
+++ Classes/Domain/Repository/ProductPriceRepository.php
@@ -2,7 +2,7 @@
 /***************************************************************
 *  Copyright notice
 *
-*  (c) 2010 Thomas "Thasmo" Deinhamer <thasmo@gmail.com>
+*  (c) 2011 Thomas "Thasmo" Deinhamer <thasmo@gmail.com>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -23,8 +23,8 @@
 ***************************************************************/
 
 /**
- * A repository for Customers
+ * A repository for Product Prices
  */
-class Tx_HypeStore_Domain_Repository_CustomerRepository extends Tx_Hype_Domain_Repository_Typo3_FrontendUserRepository {
+class Tx_HypeStore_Domain_Repository_ProductPriceRepository extends Tx_Extbase_Persistence_Repository {
 }
 ?>
