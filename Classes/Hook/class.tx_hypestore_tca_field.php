04fed0f035899c7d7a0d23054501c627859649d6
thasmo <thasmo@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-17 15:54:02 +0000

    Fixes #13138.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hype_store/trunk@43763 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Hook/class.tx_hypestore_tca_field.php
+++ Classes/Hook/class.tx_hypestore_tca_field.php
@@ -1,14 +1,9 @@
 <?php
 
-//print_r('YES!');
-
 class tx_hypestore_tca_field {
 
 	public function categories(&$field, $obj) {
-		//print_r($field);
-
-		return $obj->getSingleField_typeSelect($field['table'],$field['field'],$field['row'],&$field);
-		//return $obj->getSingleField_typeSelect_multiple($field['table'],$field['field'],$field['row'],&$field,$field['fieldConf']['config'],$field['itemFormElValue'],'');
+		return $obj->getSingleField_typeSelect($field['table'], $field['field'], $field['row'], $field);
 	}
 }
 
